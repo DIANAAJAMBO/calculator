1fe8581a66ae3d8958d1142371f6acf244b52fba
Diana <dianaajambo@gmail.com>
2023-07-06 00:44:56 +0300

    test

--- public/index.js
+++ public/index.js
@@ -1,54 +1,67 @@
-//this the client interaction to maximize the user experience acurrency of the calculator
+/* eslint-disable linebreak-style */
+/* eslint-disable no-shadow */
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-undef */
+// this the client interaction to maximize the user experience acurrency of the calculator
 
- let input1 = document.getElementById('first')
- let input2 = document.getElementById('second')
- let operator = document.getElementById('operator')
- let result = document.getElementById('screen')
- let myObject = {
-        input1: "",
-        input2: "", 
-        operator: ""
-    }
-input1.addEventListener('input', function (event) {
-    myObject.input1 = event.target.value;
-   result.value = myObject.input1+" " +myObject.operator+" "+myObject.input2;
-})
-input2.addEventListener('input', function (event) {
-    myObject.input2 = event.target.value;
-    result.value = myObject.input1+" " +myObject.operator+" "+myObject.input2;;
-})
-operator.addEventListener('input', function (event) {
-    myObject.operator = event.target.value;
-    result.value = myObject.input1+" " +myObject.operator+" "+myObject.input2;;
-})
+const input1 = document.getElementById('first');
+const input2 = document.getElementById('second');
+const operator = document.getElementById('operator');
+const result = document.getElementById('screen');
+const myObject = {
+  input1: '',
+  input2: '',
+  operator: '',
+};
+input1.addEventListener('input', (event) => {
+  myObject.input1 = event.target.value;
+  result.value = `${myObject.input1} ${myObject.operator} ${myObject.input2}`;
+});
+input2.addEventListener('input', (event) => {
+  myObject.input2 = event.target.value;
+  result.value = `${myObject.input1} ${myObject.operator} ${myObject.input2}`;
+});
+operator.addEventListener('input', (event) => {
+  myObject.operator = event.target.value;
+  result.value = `${myObject.input1} ${myObject.operator} ${myObject.input2}`;
+});
 
- function calculate(event) {
-    var input1 = parseFloat(document.getElementById('first').value);
-    var input2 = parseFloat(document.getElementById('second').value);
-    var operator = document.getElementById('operator').value;
-    var result = '';
-    
-    switch (operator) {
-        case '+':
-            result = input1 + input2;
-            break;
-        case '-':
-            result = input1 - input2;
-            break;
-        case '*':
-            result = input1 * input2;
-            break;
-        case '/':
-            result = input1 / input2;
-            break;
-        case '%':
-            result = input1 % input2;
-            break;
-        default:
-            result = 'Invalid operator';
-            event.preventDefault();
-            break;
-    }
 
-    document.getElementById('screen').value = result;
+
+function calculate(num1,operator2,num2) {
+  num1 = parseFloat(document.getElementById('first').value);
+  num2 = parseFloat(document.getElementById('second').value);
+  operator2 = document.getElementById('operator').value;
+  // let result1= '';
+  // num1=number1
+  // num2=number2
+  // operator2=operator1
+  // let result1;
+  switch (operator2) {
+    case '+':
+      result1 = num1 + num2;
+      break;
+    case '-':
+      result1 = num1 - num2;
+      break;
+    case '*':
+      result1 = num1 * num2;
+      break;
+    case '/':
+      result1 = num1 / num2;
+      break;
+    case '%':
+      result1 = num1 % num2;
+      break;
+    default:
+      result1 = 'Invalid operator';
+      return false
+      break;
+  }
+  
+  document.getElementById('screen').value = result1;
+  //return used for testing purpose
+  // return result1
 }
+
+module.exports = calculate;
---
+++ test/test.js
@@ -0,0 +1,19 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable import/no-extraneous-dependencies */
+const { assert } = require('chai');
+const calculate = require("../public/index")
+
+describe('calculator test', () =>{
+    it('1+1=2', () => {
+        assert.equal(calculate(1,"+",1), 2);
+      });
+    it('20*20=400', () => {
+        assert.equal(calculate(20,"*",20), 400);
+      });
+    it('30-10=20', () => {
+        assert.equal(calculate(30,"-",10), 20);
+      });
+    it('50/2=25', () => {
+        assert.equal(calculate(50,"/",2), 25);
+      });
+})
