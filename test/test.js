e459322a50b11261fffc0c62ea0c71c0ca309ccc
Diana <dianaajambo@gmail.com>
2023-07-09 22:00:00 +0300

    Add readme

--- test/test.js
+++ test/test.js
@@ -3,17 +3,17 @@
 // const { assert } = require('chai');
 // const calculate = require("../public/index")
 
-// describe('calculator test', () =>{
-//     it('1+1=2', () => {
-//         assert.equal(calculate(1,"+",1), 2);
-//       });
-//     it('20*20=400', () => {
-//         assert.equal(calculate(20,"*",20), 400);
-//       });
-//     it('30-10=20', () => {
-//         assert.equal(calculate(30,"-",10), 20);
-//       });
-//     it('50/2=25', () => {
-//         assert.equal(calculate(50,"/",2), 25);
-//       });
-// })
+describe('calculator test', () =>{
+    it('1+1=2', () => {
+        assert.equal(calculate(1,"+",1), 2);
+      });
+    it('20*20=400', () => {
+        assert.equal(calculate(20,"*",20), 400);
+      });
+    it('30-10=20', () => {
+        assert.equal(calculate(30,"-",10), 20);
+      });
+    it('50/2=25', () => {
+        assert.equal(calculate(50,"/",2), 25);
+      });
+})
